db803452ad259e6042e9816da0b4ba35012e6148
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-06-23 12:10:34 +0000

    implémentation du haut du dashboard

--- src/app/modules/administration/dashboard/dashboard.component.ts
+++ src/app/modules/administration/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  chiffreAffaire!: number
+  tottalTransaction!: number
+  totalEchec!: number
+  ngOnInit(): void {
+    this.chiffreAffaire= 2000000
+    this.tottalTransaction= 500
+    this.totalEchec=250
+  }
+
 
 }
