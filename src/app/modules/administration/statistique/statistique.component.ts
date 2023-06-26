c9e98539b2498553660b607322ce635d3528f081
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-06-26 16:24:42 +0000

    ajout des diifferentes catégorie de statistique

--- src/app/modules/administration/statistique/statistique.component.ts
+++ src/app/modules/administration/statistique/statistique.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-statistique',
   templateUrl: './statistique.component.html',
-  styleUrls: ['./statistique.component.scss']
+  styleUrls: ['./statistique.component.scss'],
 })
-export class StatistiqueComponent {
+export class StatistiqueComponent implements OnInit {
+  items: MenuItem[];
+  items1: MenuItem[];
+  data: any;
+  dataP: any;
+  menuSelected: number = 1;
+  leSelect: number = 1;
 
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Général',
+        command: () => {
+          this.menuSelected = 1;
+        },
+      },
+      {
+        label: 'Période',
+        command: () => {
+          this.menuSelected = 2;
+        },
+      },
+      {
+        label: 'Mode de paiement',
+        command: () => {
+          this.menuSelected = 3;
+        },
+      },
+    ];
+
+    this.items1 = [
+      {
+        label: 'Général',
+        command: () => {
+          this.menuSelected = 1;
+        },
+      },
+    ];
+    this.data = {
+      labels: ['Succes', 'Echecs'],
+      datasets: [
+        {
+          data: [75, 5],
+          backgroundColor: ['#07506A', '#EE6060'],
+          hoverBackgroundColor: ['#07506A', '#EE6060'],
+        },
+      ],
+    };
+  }
 }
