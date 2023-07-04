d2dc4c36c5eceedd7d047762c150182433459ae3
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-07-04 09:46:39 +0000

    ajout component profil

--- src/app/modules/administration/profil/profil.component.ts
+++ src/app/modules/administration/profil/profil.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss']
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  oldP:string
+  formSignUp!: FormGroup;
+  change():void{
 
+  }
 }
