ea9b3004e85f11bcd7e40ced088eee9711ce321a
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-07-04 14:41:08 +0000

    add methode qui permet a l'utilisateur de se connecter

--- src/app/modules/session/admin-login/admin-login.component.ts
+++ src/app/modules/session/admin-login/admin-login.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AdminService } from 'src/app/shared/services/admin.service';
+import { UtilisService } from 'src/app/shared/services/utilis.service';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
   styleUrls: ['./admin-login.component.scss'],
+  providers: [MessageService]
+
 })
 export class AdminLoginComponent implements OnInit {
   value!: string;
@@ -15,7 +21,11 @@ export class AdminLoginComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private route: Router
+    private route: Router,
+    private adminService: AdminService,
+    private utilis: UtilisService,
+    private messageService: MessageService,
+
     ) {}
 
   ngOnInit(): void {
@@ -25,22 +35,52 @@ export class AdminLoginComponent implements OnInit {
   // Construction du formulaire
   buildForm(){
     this.formSignUp = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,100}$')]],
+      username: ['', [Validators.required]],
       password: ['', [Validators.required]]
     })
   }
 
   // Méthode permetant un chargement de 2s avant la validation du formulaire
-  load() {
-    this.loading = true;
-    
-    setTimeout(() => {
-      this.loading = false;
-      this.login()
-    }, 2000);
+  handleOk() {
+    let res= this.formSignUp.value
+    this.login(res)
+    // setTimeout(() => {
+      
+    // }, 2000);
   }
 
-  login(){
-    this.route.navigate(['administration'])
+  login(obj:any){
+    this.loading = true;
+    this.adminService.login(obj).subscribe({
+      next: data=>{
+        this.utilis.response(data, (d:any)=>{
+          this.loading = false
+          console.log(d)
+          if(data.status==204){
+            this.loading=false
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Attention',
+              detail: 'Mettez les bons identifiants',
+            })
+          }
+          else{
+            localStorage.setItem('user_info',JSON.stringify(d))
+            this.route.navigate(['administration'])
+          }
+         
+        })
+      },
+      error: (error) => {
+        this.utilis.response(error,(d:any)=>{
+          this.loading=false
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Attention',
+            detail: 'Mettez les bons identifiants',
+          })
+        })
+      }
+    })
   }
 }
