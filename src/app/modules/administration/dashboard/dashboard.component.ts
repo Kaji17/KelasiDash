39089c8f09eb344b63a07b9110830777808df1ed
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-07-14 12:19:55 +0000

    modification statistique methode

--- src/app/modules/administration/dashboard/dashboard.component.ts
+++ src/app/modules/administration/dashboard/dashboard.component.ts
@@ -1,53 +1,212 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { Configurable } from 'src/app/core/config';
+import { StatistiqueService } from 'src/app/shared/services/statistique.service';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit{
-
+export class DashboardComponent implements OnInit, OnDestroy {
   data: any;
-
   chartOptions: any;
-
   subscription: Subscription;
-
-  // config: AppConfig;
-  chiffreAffaire!: number
-  tottalTransaction!: number
-  totalEchec!: number
+  subscriptionCount: Subscription;
+  chiffreAffaire!: number;
+  tottalTransaction!: any;
+  tottalTransactionSuccess!: number;
+  tottalTransactionEchec!: number;
+  tottalTransactionPending!: number;
+  tottalTransactionSuscriber!: number;
+  tottalTransactionProblem!: number;
+  TT!: number;
+  totalEchec!: number;
   progressMomo!: number;
   progressUssd!: number;
-  titleAppMobile!: string
-  titleUSSD!: string
+  titleAppMobile!: string;
+  titleUSSD!: string;
+  CA: number;
+  objChiffreaffaire: {};
+  countCA: any;
+  closable: boolean;
+  dateDebut: Date;
+  iconsverif: string = 'pi pi-check';
+
+  constructor(
+    private configService: Configurable,
+    private statistiqueService: StatistiqueService
+  ) {}
+
   ngOnInit(): void {
-    this.chiffreAffaire= 2000000
-    this.tottalTransaction= 500
-    this.progressMomo= 70
-    this.progressUssd=30
-    this.totalEchec=250
-    this.titleAppMobile= 'Résumé transaction App mobile'
-    this.titleUSSD= 'Résumé transaction USSD'
+    this.getChiffreAffaireMomo();
+
+    this.objChiffreaffaire = {};
+    this.getTotalTransaction();
+    this.getChiffreAffaireA();
+    this.getTotalTransactionM();
+    this.tottalTransactionSuccess = 0;
+    this.tottalTransactionEchec = 0;
+    this.tottalTransactionPending = 0;
+    this.tottalTransactionProblem = 0;
+    this.tottalTransactionSuscriber = 0;
+    this.getTotalTransactionEchec();
+    this.tottalTransaction = 0;
+    this.CA = 0;
+    this.chiffreAffaire = 0;
+    this.TT = 0;
+    this.progressMomo = 70;
+    this.progressUssd = 30;
+    this.totalEchec = 250;
+    this.titleAppMobile = 'Résumé transaction App mobile';
+    this.titleUSSD = 'Résumé transaction USSD';
     this.data = {
-      datasets: [{
-          data: [
-              11,
-              16
-          ],
-          backgroundColor: [
-              "#EE6060",
-              "#39A74B",
-          ],
-          label: 'My dataset'
-      }],
-      labels: [
-          "Echec",
-          "Succes",
-      ]
-  };
+      datasets: [
+        {
+          data: [11, 16],
+          backgroundColor: ['#EE6060', '#39A74B'],
+          label: 'My dataset',
+        },
+      ],
+      labels: ['Echec', 'Succes'],
+    };
+  }
+  countagge() {
+    this.countCA = setInterval(() => {
+      this.verify();
+    }, 1);
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    this.subscriptionCount.unsubscribe();
   }
 
+  // Fonction donnant un effet de compte au chiffre d'affaire
+  verify() {
+    if (this.chiffreAffaire !== 0 && this.CA < this.chiffreAffaire) {
+      this.CA = this.CA + 1000;
+      if (this.CA == this.chiffreAffaire) {
+        clearInterval(this.countCA);
+      }
+    } else {
+      this.CA = this.chiffreAffaire;
+    }
+  }
 
+  // Recuperer le nombre total de toute les transactions
+  getTotalTransaction() {
+    console.log('YOYO',this.getTotalTransactionM())
+    this.subscriptionCount = this.statistiqueService
+      .getTransactionTotal()
+      .subscribe({
+        next: (total) => {
+          console.log('Total__Trans:', total);
+          this.tottalTransaction = total;
+        },
+      });
+  }
+
+  // Recuperer le nombre total des transaction par status
+  getTotalTransactionStatus(obj: any): number {
+    let valeur: number;
+    this.statistiqueService.getTransactionTotal(obj).subscribe({
+      next: (total) => {
+        console.log(`Total__Trans_${obj.statut}:`, total);
+        // this.tottalTransactionSuccess = total;
+        valeur = total;
+      },
+    });
+    return valeur;
+  }
+
+  // Recuperer le nombre total des transaction par reussit
+  getTotalTransactionSuccess() {
+    let ca = {
+      statut: 'Success',
+    };
+   
+    this.tottalTransactionSuccess = this.getTotalTransactionStatus(ca);
+  }
+
+  getTotalTransactionEchec() {
+    let ca1 = {
+      statut: 'Pending',
+    };
+    let ca2 = {
+      statut: 'Problem with getting account details.',
+    };
+    let ca3 = {
+      statut: 'Subscriber has insufficient balance',
+    };
+    let val1;
+    let val2;
+    let val3;
+    val1 = this.getTotalTransactionStatus(ca1);
+    val2 = this.getTotalTransactionStatus(ca2);
+    val3 = this.getTotalTransactionStatus(ca3);
+
+    this.tottalTransactionEchec = val1 + val2 + val3;
+  }
+
+  // Recuperer le nombre total de toute les transactions par MoMo
+  getTotalTransactionM() {
+    this.statistiqueService.getTransactionTotalMomo().subscribe({
+      next: (value) => {
+        console.log(`Transaction MoMo, ${value}`);
+      },
+    });
+  }
+
+  // Récuperation du chiffre d'affaire d'Airtimes des transaction réussit
+  getChiffreAffaireA(obj?: {}) {
+    let ca = {
+      datedebut: '',
+      datefin: '',
+      statut: 'Success',
+    };
+    ca.datefin = this.formatDateTime(new Date());
+
+    console.log(ca);
+    this.subscription = this.statistiqueService.getMontantTotal(ca).subscribe({
+      next: (value) => {
+        this.chiffreAffaire = value;
+        console.log('montantTottal', this.chiffreAffaire);
+      },
+    });
+    this.countagge();
+  }
+
+  // Récuperation du chiffre d'affaire de MoMo des transaction réussit
+  getChiffreAffaireMomo(obj?: {}) {
+    let ca = {
+
+      statut: 'SUCCESSFUL',
+    };
+    // ca.datefin = this.formatDateTime(new Date());
+
+    this.statistiqueService.getMontantTotalMomo(ca).subscribe({
+      next: (value) => {
+        console.log('montant Momo', value);
+      },
+    });
+  }
+
+  // Formatage de la date en yyyy-MM-dd HH:mm:ss.SSS
+  formatDateTime(date: Date): string {
+    const year = date.getFullYear();
+    const month = this.padZero(date.getMonth() + 1);
+    const day = this.padZero(date.getDate());
+    const hours = this.padZero(date.getHours());
+    const minutes = this.padZero(date.getMinutes());
+    const seconds = this.padZero(date.getSeconds());
+    const milliseconds = this.padZero(date.getMilliseconds(), 3);
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+  }
+
+  padZero(value: number, length: number = 2): string {
+    return value.toString().padStart(length, '0');
+  }
 }
