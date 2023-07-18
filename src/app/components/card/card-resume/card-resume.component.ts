59681a4ca500a738c9750f5b8ff698de67491b13
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-07-18 11:19:18 +0000

    ajout variable pour des card jaune ou bleu

--- src/app/components/card/card-resume/card-resume.component.ts
+++ src/app/components/card/card-resume/card-resume.component.ts
@@ -4,23 +4,52 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-card-resume',
   templateUrl: './card-resume.component.html',
-  styleUrls: ['./card-resume.component.scss']
+  styleUrls: ['./card-resume.component.scss'],
 })
-export class CardResumeComponent implements OnInit{
+export class CardResumeComponent implements OnInit {
+  @Input() total2!: number;
+  @Input() total1!: number;
+  @Input() typeCard!: string;
+  @Input() title!: string;
 
-  @Input()total2!: number
-  @Input()total1!: number
-  @Input()typeCard!: string
-  @Input()title!: string
+  cardStyle: string
+  bgStyle: string
+  lineB: string
 
-  constructor(private route: Router){}
+  constructor(private route: Router) {}
 
   ngOnInit(): void {
+    switch (this.typeCard) {
+      case 'green':
+        this.cardStyle  = 'card-green'
+        this.bgStyle  = 'bGreen bGreen-l'
+        this.lineB  = 'greenLineB'
+        break;
 
+      case 'red':
+        this.cardStyle  = 'card-red'
+        this.bgStyle  = 'bRed bRed-l'
+        this.lineB  = 'redLineB'
+        break;
+
+      case 'blue':
+        this.cardStyle  = 'card-blue'
+        this.bgStyle  = 'bBlue bBlue-l'
+        this.lineB  = 'blueLineB'
+        break;
+
+      case 'yellow':
+        this.cardStyle  = 'card-yellow'
+        this.bgStyle  = 'bYellow bYellow-l'
+        this.lineB  = 'yellowLineB'
+        break;
+
+      default:
+        break;
+    }
   }
 
-
-  onStatNav(): void{
-    this.route.navigate(['/administration/statistiques'])
+  onStatNav(): void {
+    this.route.navigate(['/administration/statistiques']);
   }
 }
