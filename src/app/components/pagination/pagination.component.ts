cd58550dd4213f421e839e4653fb8febb463dff7
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-07-20 12:13:12 +0000

    pagination ok

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent {
+  page:number=0
+  rows:number = 5
+  // total:number=0
+  @Input() total = 0
+  options:any=[
+    { label: 5, value: 5 },
+    { label: 10, value: 10 },
+    { label: 20, value: 20 },
+    { label: 120, value: 120 }
+  ]
+  first:number = 0
+
+  @Output() newItemEvent = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onPageChange(event:any){
+    // console.log(event)
+    this.addNewItem({pagination: true,page:event.page,size:event.rows})
+   
+  }
+
+  onElementSelectChange(event:any){
+    // console.log(event)
+    this.addNewItem({pagination: true,page:0,size:event.value}) 
+   
+  }
+
+  addNewItem(value: any) { 
+      this.newItemEvent.emit(value);
+  }
+
 
 }
